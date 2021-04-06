3d8a6e7e565647daae28933c3699e3fe2d0027b6
ZmjDns <292570927@qq.com>
2021-04-06 22:51:35 +0800

    transFermodeView

--- app/src/main/java/com/zmj/relearnandroid/view/Extensions.kt
+++ app/src/main/java/com/zmj/relearnandroid/view/Extensions.kt
@@ -1,7 +1,11 @@
 package com.zmj.relearnandroid.view
 
+import android.content.Context
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.TypedValue
+import com.zmj.relearnandroid.R
 
 /**
  * Author : Zmj
@@ -12,3 +16,18 @@ import android.util.TypedValue
  */
 val Int.dp
     get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this.toFloat(),Resources.getSystem().displayMetrics)
+
+/**
+ * 按照一定的比例去获取图片的Bitmap
+ */
+fun getAvatar(context: Context,width: Int): Bitmap {
+    val options = BitmapFactory.Options()
+    options.inJustDecodeBounds = true
+//    BitmapFactory.decodeResource(Resources.getSystem(),R.drawable.avatar_rengwuxian,options)
+    BitmapFactory.decodeResource(context.resources,R.drawable.avatar_rengwuxian,options)
+    options.inJustDecodeBounds = false
+    options.inDensity = options.outWidth
+    options.inTargetDensity = width
+//    return BitmapFactory.decodeResource(Resources.getSystem(),R.drawable.avatar_rengwuxian,options)
+    return BitmapFactory.decodeResource(context.resources,R.drawable.avatar_rengwuxian,options)
+}
---
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson12Xfermode/AvatarView.kt
@@ -0,0 +1,37 @@
+package com.zmj.relearnandroid.view.lesson12Xfermode
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import android.view.WindowInsetsAnimation
+import com.zmj.relearnandroid.view.dp
+import com.zmj.relearnandroid.view.getAvatar
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2021-04-06
+ * Description :
+ */
+class AvatarView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+    private val IMAGE_PADDING = 20.dp
+    private val IMAGE_WIDTH = 200.dp
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var bitmap: Bitmap = getAvatar(context,IMAGE_WIDTH.toInt())
+    private val xFermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+    private val bounds = RectF(IMAGE_PADDING,IMAGE_PADDING, IMAGE_PADDING + IMAGE_WIDTH, IMAGE_PADDING + IMAGE_WIDTH)
+
+
+    override fun onDraw(canvas: Canvas) {
+
+        val count =  canvas.saveLayer(bounds,null)   //离屏缓冲，否则无法画出圆形头像
+        canvas.drawOval(IMAGE_PADDING,IMAGE_PADDING, IMAGE_PADDING + IMAGE_WIDTH, IMAGE_PADDING + IMAGE_WIDTH,paint)
+        paint.xfermode = xFermode
+        canvas.drawBitmap(bitmap,IMAGE_PADDING,IMAGE_PADDING,paint)
+        paint.xfermode = null
+        canvas.restoreToCount(count)    //恢复离屏缓冲
+    }
+}
