f56934953bed98a02e24ccc70305fb20b10e12b9
ZmjDns <292570927@qq.com>
2021-04-08 22:31:57 +0800

    show XFermodeView

---
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson12Xfermode/XfermodeView.kt
@@ -0,0 +1,27 @@
+package com.zmj.relearnandroid.view.lesson12Xfermode
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Author : Zmj
+ * Blog : https://blog.csdn.net/Zmj_Dns
+ * GitHub : https://github.com/ZmjDns
+ * Time : 2021-04-07
+ * Description :
+ */
+class XfermodeView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas: Canvas) {
+        paint
+
+
+    }
+
+}
