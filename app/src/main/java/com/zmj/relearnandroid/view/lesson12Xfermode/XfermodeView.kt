2494b299f481818774709088b9ba1f838ec9ca1d
ZmjDns <292570927@qq.com>
2021-04-09 23:17:49 +0800

    XFermodeView

--- app/src/main/java/com/zmj/relearnandroid/view/lesson12Xfermode/XfermodeView.kt
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson12Xfermode/XfermodeView.kt
@@ -1,11 +1,10 @@
 package com.zmj.relearnandroid.view.lesson12Xfermode
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.zmj.relearnandroid.view.dp
 
 /**
  * Author : Zmj
@@ -17,10 +16,30 @@ import android.view.View
 class XfermodeView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val bounds = RectF(150f,50f,300.dp,200.dp)
+    private val xFermode = PorterDuffXfermode(PorterDuff.Mode.SRC)
+
+    private val circelBitmap = Bitmap.createBitmap(150.dp.toInt(),150.dp.toInt(),Bitmap.Config.ARGB_8888)
+    private val squareBitmap = Bitmap.createBitmap(150.dp.toInt(),150.dp.toInt(),Bitmap.Config.ARGB_8888)
+
+    init {
+        val canvas = Canvas(circelBitmap)
+        paint.color = Color.parseColor("#D81B60")
+        canvas.drawOval(50.dp,0.dp,150.dp,100.dp,paint)
+        canvas.setBitmap(squareBitmap)
+        paint.color = Color.BLUE
+        canvas.drawRect(0.dp,50.dp,100.dp,150.dp, paint)
+
+    }
 
     override fun onDraw(canvas: Canvas) {
-        paint
+        val count = canvas.saveLayer(bounds, null)
 
+        canvas.drawBitmap(circelBitmap,75.dp,50.dp,paint)
+        paint.xfermode = xFermode
+        canvas.drawBitmap(circelBitmap,75.dp,50.dp,paint)
+        paint.xfermode = null
+        canvas.restoreToCount(count)
 
     }
 
