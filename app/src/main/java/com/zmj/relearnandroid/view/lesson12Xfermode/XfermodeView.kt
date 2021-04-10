dd3780ecf73af2e2793fccfb003290e0c9594a18
ZmjDns <292570927@qq.com>
2021-04-10 12:16:34 +0800

    Xfermode多种状态实现

--- app/src/main/java/com/zmj/relearnandroid/view/lesson12Xfermode/XfermodeView.kt
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson12Xfermode/XfermodeView.kt
@@ -16,8 +16,8 @@ import com.zmj.relearnandroid.view.dp
 class XfermodeView(context: Context, attrs: AttributeSet): View(context, attrs) {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val bounds = RectF(150f,50f,300.dp,200.dp)
-    private val xFermode = PorterDuffXfermode(PorterDuff.Mode.SRC)
+    private val bounds = RectF(150f,50f,300.dp,200.dp)  //  离屏缓冲区域，背景和目标存在的区域
+    private val xFermode = PorterDuffXfermode(PorterDuff.Mode.SRC_OUT)  // SRC_IN   SRC_OUT。。。
 
     private val circelBitmap = Bitmap.createBitmap(150.dp.toInt(),150.dp.toInt(),Bitmap.Config.ARGB_8888)
     private val squareBitmap = Bitmap.createBitmap(150.dp.toInt(),150.dp.toInt(),Bitmap.Config.ARGB_8888)
@@ -29,15 +29,14 @@ class XfermodeView(context: Context, attrs: AttributeSet): View(context, attrs) 
         canvas.setBitmap(squareBitmap)
         paint.color = Color.BLUE
         canvas.drawRect(0.dp,50.dp,100.dp,150.dp, paint)
-
     }
 
     override fun onDraw(canvas: Canvas) {
-        val count = canvas.saveLayer(bounds, null)
+        val count = canvas.saveLayer(bounds, null)  //设置离屏缓冲
 
-        canvas.drawBitmap(circelBitmap,75.dp,50.dp,paint)
+        canvas.drawBitmap(circelBitmap,150.dp,50.dp,paint)
         paint.xfermode = xFermode
-        canvas.drawBitmap(circelBitmap,75.dp,50.dp,paint)
+        canvas.drawBitmap(squareBitmap,150.dp,50.dp,paint)
         paint.xfermode = null
         canvas.restoreToCount(count)
 
