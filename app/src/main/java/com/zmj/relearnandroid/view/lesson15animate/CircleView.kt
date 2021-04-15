f36b4c907f310f3cd5acd181618a6534d84334d6
ZmjDns <292570927@qq.com>
2021-04-15 10:48:26 +0800

    init circleView

--- app/src/main/java/com/zmj/relearnandroid/view/lesson15animate/CircleView.kt
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson15animate/CircleView.kt
@@ -2,9 +2,11 @@ package com.zmj.relearnandroid.view.lesson15animate
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.zmj.relearnandroid.view.dp
 
 /**
  * Author : Zmj
@@ -15,12 +17,14 @@ import android.view.View
  */
 class CircleView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.RED
+    }
     private var radius = 50.dp
 
 
 
     override fun onDraw(canvas: Canvas) {
-
+        canvas.drawOval(width/2-radius,height/2-radius,width/2+radius,height/2+radius,paint)
     }
 }
