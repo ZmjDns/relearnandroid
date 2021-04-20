09b9f65bb0b39a4dc9bba17de28120dd010bc840
ZmjDns <292570927@qq.com>
2021-04-20 14:20:43 +0800

    MaterialEditText entrance

--- app/src/main/java/com/zmj/relearnandroid/view/lesson15animate/CircleView.kt
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson15animate/CircleView.kt
@@ -19,12 +19,46 @@ class CircleView(context: Context?, attrs: AttributeSet?) : View(context, attrs)
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         color = Color.RED
+        strokeWidth = 20.dp
+        style = Paint.Style.STROKE
     }
-    private var radius = 50.dp
-
+    private val paint2 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.YELLOW
+        strokeWidth = 20.dp
+        style = Paint.Style.STROKE
+    }
+    private val paint3 = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.GREEN
+        strokeWidth = 20.dp
+        style = Paint.Style.STROKE
+    }
+    private var radius = 100.dp
+        set(value) {
+            field = value
+            invalidate()
+        }
+    private var sweepAngle = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
 
 
     override fun onDraw(canvas: Canvas) {
-        canvas.drawOval(width/2-radius,height/2-radius,width/2+radius,height/2+radius,paint)
+//        canvas.drawOval(width/2-radius,height/2-radius,width/2+radius,height/2+radius,paint)
+
+        if (sweepAngle <360) {
+            canvas.save()
+            canvas.drawArc(width/2-radius,height/2-radius,width/2+radius,height/2+radius,0f,sweepAngle,false,paint)
+            canvas.restore()
+        } else if (sweepAngle >= 360 && sweepAngle < 720) {
+            canvas.save()
+            canvas.drawArc(width/2-radius,height/2-radius,width/2+radius,height/2+radius,360f,sweepAngle,false,paint2)
+            canvas.restore()
+        } else if (sweepAngle >= 720) {
+            canvas.save()
+            canvas.drawArc(width/2-radius,height/2-radius,width/2+radius,height/2+radius,720f,sweepAngle,false,paint3)
+            canvas.restore()
+        }
     }
 }
---
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson17materialtext/MaterialEditTextAct.kt
@@ -0,0 +1,12 @@
+package com.zmj.relearnandroid.view.lesson17materialtext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zmj.relearnandroid.R
+
+class MaterialEditTextAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_material_editext)
+    }
+}
