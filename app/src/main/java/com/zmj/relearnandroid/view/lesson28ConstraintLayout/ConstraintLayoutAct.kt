5ba0f048de343cbb5945e6116abfb4e292c9cdd4
ZmjDns <292570927@qq.com>
2021-04-20 14:25:30 +0800

    ConstraintLayout entrance

---
+++ app/src/main/java/com/zmj/relearnandroid/view/lesson28ConstraintLayout/ConstraintLayoutAct.kt
@@ -0,0 +1,12 @@
+package com.zmj.relearnandroid.view.lesson28ConstraintLayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zmj.relearnandroid.R
+
+class ConstraintLayoutAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
